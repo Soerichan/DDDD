fab796834dce3fe29642356645ff3811cda92abd
Jake Lim <eoakdhkd1171@gmail.com>
2023-01-03 21:03:24 +0900

    feat: MapMaker,MapGird 등 절차적 맵 생성 구현

---
+++ Assets/Scripts/Map/MapGrid.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGrid : MonoBehaviour
+{
+    [SerializeField]
+    private MapRoomButton MapButton01;
+    [SerializeField]
+    private MapRoomButton MapButton02;
+    [SerializeField]
+    private MapRoomButton MapButton03;
+    [SerializeField]
+    private MapRoomButton MapButton04;
+    [SerializeField]
+    private MapRoomButton MapButton05;
+    [SerializeField]
+    private MapRoomButton MapButton06;
+    [SerializeField]
+    private MapRoomButton MapButton07;
+
+    public MapMaker mapMaker;
+
+    [SerializeField]
+    private Vector2Int gridSize;
+    [SerializeField]
+    private float placeSize;
+
+    private void Start()
+    {
+        for (int y = 0; y < gridSize.y; y++)
+        {
+            for (int x = 0; x < gridSize.x; x++)
+            {
+                if(0==mapMaker.MapArray[x,y])
+                {
+                    continue;
+                }
+
+                if(x==2&&y==9)
+                {
+                    MapRoomButton buttonPlace = Instantiate(MapButton07, transform);
+                    buttonPlace.transform.localPosition = new Vector2((x + 0.5f) * placeSize, (y + 0.5f) * placeSize);
+                    continue;
+                }
+
+                int randomRoom = UnityEngine.Random.Range(1, 6);
+
+                switch (randomRoom)
+                {
+                    case 1:
+                        {
+                            MapRoomButton buttonPlace = Instantiate(MapButton01, transform);
+                            buttonPlace.transform.localPosition = new Vector2((x + 0.5f) * placeSize, (y + 0.5f) * placeSize);
+                            break;
+                        }
+                    case 2:
+                        {
+                            MapRoomButton buttonPlace = Instantiate(MapButton02, transform);
+                            buttonPlace.transform.localPosition = new Vector2((x + 0.5f) * placeSize, (y + 0.5f) * placeSize);
+                            break;
+                        }
+                    case 3:
+                        {
+                            MapRoomButton buttonPlace = Instantiate(MapButton03, transform);
+                            buttonPlace.transform.localPosition = new Vector2((x + 0.5f) * placeSize, (y + 0.5f) * placeSize);
+                            break;
+                        }
+                
+                    case 4:
+                        {
+                            MapRoomButton buttonPlace = Instantiate(MapButton04, transform);
+                            buttonPlace.transform.localPosition = new Vector2((x + 0.5f) * placeSize, (y + 0.5f) * placeSize);
+                            break;
+                        }
+                    case 5:
+                        {
+                            MapRoomButton buttonPlace = Instantiate(MapButton05, transform);
+                            buttonPlace.transform.localPosition = new Vector2((x + 0.5f) * placeSize, (y + 0.5f) * placeSize);
+                            break;
+                        }
+                    case 6:
+                        {
+                            MapRoomButton buttonPlace = Instantiate(MapButton06, transform);
+                            buttonPlace.transform.localPosition = new Vector2((x + 0.5f) * placeSize, (y + 0.5f) * placeSize);
+                            break;
+                        }
+                    default:
+                        continue;
+                }
+                
+
+                //  mapMaker.
+                //  MapRoomButton buttonPlace = Instantiate(MapButton, transform);
+                //buttonPlace.transform.localPosition = new Vector2((x + 0.5f) * placeSize,(y + 0.5f) * placeSize);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.matrix = transform.localToWorldMatrix;
+
+        for (int y = 0; y < gridSize.y; y++)
+        {
+            for (int x = 0; x < gridSize.x; x++)
+            {
+                var position = new Vector2((x + 0.5f) * placeSize, (y + 0.5f) * placeSize);
+                Gizmos.DrawWireCube(position, new Vector2(placeSize, placeSize));
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Map/MapMaker.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapMaker : MonoBehaviour
+{
+    public int[,] MapArray = new int[5, 10];
+    bool IsFrontCompartmentWithi;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MapMake();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void MapMake()
+    {
+        MapArray[1, 0] = 1;
+        MapArray[2, 0] = 1;
+        MapArray[3, 0] = 1;
+        MapArray[2, 9] = 1;
+
+        for (int j = 1; j < 9; j++)
+        {
+     
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                int randomMapping = UnityEngine.Random.Range(-1, 1);
+
+                if (MapArray[i,j-1]==1)
+                {
+                    int MappingResult = i + randomMapping;
+
+                    if (MappingResult < 0)
+                    {
+                        MappingResult = 0;
+                        MapArray[MappingResult+1, j] = 1;
+                    }
+                    if(MappingResult>4)
+                    {
+                        MappingResult = 4;
+                        MapArray[MappingResult-1, j] = 1;
+                    }
+
+                    MapArray[MappingResult, j] = 1;
+
+                }
+
+
+            }
+        }
+    }
+}
