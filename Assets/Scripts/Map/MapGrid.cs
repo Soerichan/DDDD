fe0b68396d7211ca59bac507868d0592e6174ad2
Jake Lim <eoakdhkd1171@gmail.com>
2023-01-04 17:19:25 +0900

    feat: map의 경로 구현

--- Assets/Scripts/Map/MapGrid.cs
+++ Assets/Scripts/Map/MapGrid.cs
@@ -19,6 +19,17 @@ public class MapGrid : MonoBehaviour
     [SerializeField]
     private MapRoomButton MapButton07;
 
+    [SerializeField]
+    private MapRoad Road01;
+    [SerializeField]
+    private MapRoad Road02;
+    [SerializeField]
+    private MapRoad Road03;
+    [SerializeField]
+    private MapRoad Road04;
+    [SerializeField]
+    private MapRoad Road05;
+
     public MapMaker mapMaker;
 
     [SerializeField]
@@ -30,6 +41,78 @@ public class MapGrid : MonoBehaviour
     {
         for (int y = 0; y < gridSize.y; y++)
         {
+            if (y == gridSize.y - 1)
+            {
+
+                if (mapMaker.MapArray[0, y - 1] == 1)
+                {
+                    MapRoad maproad = Instantiate(Road04, transform);
+                    maproad.transform.localPosition = new Vector2((0 + 1.4f) * placeSize, (y + 0.05f) * placeSize);
+                }
+
+                if (mapMaker.MapArray[gridSize.x-1, y - 1] == 1)
+                {
+                    MapRoad maproad = Instantiate(Road05, transform);
+                    maproad.transform.localPosition = new Vector2((4 -0.4f) * placeSize, (y + 0.05f) * placeSize);
+                }
+
+            }
+
+            for (int x = 0; x < gridSize.x; x++)
+            {
+                
+
+                if (y > 0)
+                {
+                    if (mapMaker.MapArray[x, y - 1] == 1 && mapMaker.MapArray[x, y] == 1)
+                    {
+                        MapRoad maproad = Instantiate(Road01, transform);
+                        maproad.transform.localPosition = new Vector2((x + 0.5f) * placeSize, (y+0.05f) * placeSize);
+                    }
+
+                    if (x > 1)
+                    {
+
+                        if (mapMaker.MapArray[x - 1, y - 1] == 1 && mapMaker.MapArray[x, y] == 1)
+                        {
+                            MapRoad maproad = Instantiate(Road02, transform);
+                            maproad.transform.localPosition = new Vector2((x) * placeSize, (y + 0.05f) * placeSize);
+                        }
+                    }
+                    else if(x==0)
+                    {
+                        if (mapMaker.MapArray[0, y - 1] == 1 && mapMaker.MapArray[1, y] == 1)
+                        {
+                            MapRoad maproad = Instantiate(Road02, transform);
+                            maproad.transform.localPosition = new Vector2((x+1f) * placeSize, (y + 0.05f) * placeSize);
+                        }
+                    }
+
+                    if (x < gridSize.x - 1)
+                    {
+                        if (mapMaker.MapArray[x + 1, y - 1] == 1 && mapMaker.MapArray[x, y] == 1)
+                        {
+                            MapRoad maproad = Instantiate(Road03, transform);
+                            maproad.transform.localPosition = new Vector2((x+1f) * placeSize, (y + 0.05f) * placeSize);
+                        }
+                    }
+                    else if(x== gridSize.x - 1)
+                    {
+
+                        if (mapMaker.MapArray[gridSize.x - 1, y - 1] == 1 && mapMaker.MapArray[gridSize.x - 1, y] == 1)
+                        {
+                            MapRoad maproad = Instantiate(Road03, transform);
+                            maproad.transform.localPosition = new Vector2((x) * placeSize, (y + 0.05f) * placeSize);
+                        }
+                    }
+
+                }
+            }
+        }
+
+
+                for (int y = 0; y < gridSize.y; y++)
+        {
             for (int x = 0; x < gridSize.x; x++)
             {
                 if(0==mapMaker.MapArray[x,y])
@@ -37,12 +120,15 @@ public class MapGrid : MonoBehaviour
                     continue;
                 }
 
-                if(x==2&&y==9)
+              
+
+                if (x==2&&y==9)
                 {
                     MapRoomButton buttonPlace = Instantiate(MapButton07, transform);
                     buttonPlace.transform.localPosition = new Vector2((x + 0.5f) * placeSize, (y + 0.5f) * placeSize);
                     continue;
                 }
+
 
                 int randomRoom = UnityEngine.Random.Range(1, 7);
 
@@ -88,7 +174,8 @@ public class MapGrid : MonoBehaviour
                     default:
                         continue;
                 }
-                
+
+              
 
                 //  mapMaker.
                 //  MapRoomButton buttonPlace = Instantiate(MapButton, transform);
