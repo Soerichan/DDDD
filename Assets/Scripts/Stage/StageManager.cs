00c27318e2c1063970cf00a4867bc287f47a8898
Jake Lim <eoakdhkd1171@gmail.com>
2023-01-05 21:50:42 +0900

    (stash)

--- Assets/Scripts/Stage/StageManager.cs
+++ Assets/Scripts/Stage/StageManager.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class StageManager : Singleton<StageManager>
 {
+
+   public enum RoomType { None,Room_RampageGuys, Room_Anvil, Room_HumanWave, Room_SturdyHunks, Room_Danger, Room_Cookie, Room_BOSS };
     public PlayerController m_player;
     public float m_playerNowHP;
     public Vector3 PlayerPosition;
 
     public bool m_bIsGameOver;
+    private void Awake()
+    {
+        RoomType room=RoomType.None;
+    }
     // Update is called once per frame
     void Update()
     {
